3db0aee4e276f52b33ab18da544e5b82a5ba1ce3
rvs6719 <rogozinvitaliy@mail.ru>
2025-01-11 02:20:36 +0500

    backtofront

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 # backend/main.py
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 from .routes import videos
 
 app = FastAPI()
 
 app.include_router(videos.router)
 
-@app.get("/")
+# Обслуживание статических файлов
+app.mount("/static", StaticFiles(directory="../frontend/build/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
 async def read_root():
-    return {"message": "Welcome to the YouTube Clone API!"}
+    with open("../frontend/build/index.html") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
+
+@app.get("/{full_path:path}", response_class=HTMLResponse)
+async def serve_spa(full_path: str):
+    with open("../frontend/build/index.html") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
